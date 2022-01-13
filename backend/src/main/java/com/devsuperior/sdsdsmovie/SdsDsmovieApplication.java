b1dea83eeb486f7d10541955c45aeb4b887c3d27
Gabriel de Oliveira <br.gabrieldeoliveira@gmail.com>
2022-01-13 14:25:34 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/sdsdsmovie/SdsDsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsdsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsDsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsDsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/sdsdsmovie/SdsDsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsdsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsDsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
