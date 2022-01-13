21ffc8195773db9534d5db15dfdeb67a0e9a9f62
Gabriel de Oliveira <br.gabrieldeoliveira@gmail.com>
2022-01-13 15:31:06 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./styles.css";
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/gabriel-olv">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p>/gabriel-olv</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
